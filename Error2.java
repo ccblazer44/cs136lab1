10bebdc55ac10b0a835fe835b9dcf42cdb8b9d9d
Christopher Charles Blazer <cb367@students.froot.nau.edu>
2017-01-26 11:43:59 -0700

    initial files

---
+++ Error2.java
@@ -0,0 +1,55 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Error2 {
+    private int secret;
+    private Random rand;
+
+    public Error2(int secret) {
+	this.secret = secret;
+	rand = new Random();
+    }
+
+    public void interrogate() {
+	taunt();
+	char guess = nextGuess();
+	while guess != 'q' {
+	    if(checkGuess(guess)) {
+		giveUp();
+		return;
+	    } else {
+		taunt();
+		guess = nextGuess();
+		int secret = rand.nextInt(10);
+	    }
+	}
+	taunt();
+    }
+
+    public void taunt() {
+	String taunts[] = {"I've got a secret. Na-na!",
+			   "You're dumb, you'll never guess my secret!",
+			   "Tee-hee. I'll never tell you my secret!",
+			   "My secret is so cool, and you'll never know it!"};
+	System.out.println("\n" + taunts[rand.nextInt(4)]);
+    }
+
+    public char nextGuess() {
+	System.out.print("Guess: ");
+	Scanner scanner = new Scanner(System.in);
+	return (char) scanner.nextByte();
+    }
+    
+    public boolean checkGuess(char guess) {
+	return secret == guess;
+    }
+
+    public void giveUp() {
+	System.out.println("Aw man, you guessed my secret. I guess I'll just die now...");
+    }
+
+    public static void main(String args[]) {
+	Error2 pwas = new Error2(3);
+	pwas.interrogate();
+    }
+}
